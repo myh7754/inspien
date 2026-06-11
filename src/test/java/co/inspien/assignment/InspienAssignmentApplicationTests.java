3bdbf1c3cdea2c7f36cdb3706d24c5d2cfcc5a8d
myh <myh4755@gmail.com>
2026-06-11 22:50:58 +0900

    chore: Spring Boot 4.1 / Java 21 프로젝트 스켈레톤 + 과제 의존성(XML, FTP) 추가

---
+++ src/test/java/co/inspien/assignment/InspienAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package co.inspien.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspienAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
